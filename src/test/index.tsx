bbdb6fe327fac16e558f5a9368bc4c2a75127959
jeongkyojun <kyojun2002@naver.com>
2022-12-03 15:46:07 +0900

    feat: 초기세팅 설정

---
+++ src/test/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Test = () => {
+  const clickEvent = () => {
+    console.log("click");
+  }
+  return (
+    <>
+      <h1> test sheet </h1>
+    </>
+  );
+}
+
+export default Test;
