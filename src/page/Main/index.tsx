1f780fdcd359962269b8caacf3cd89a990b66f95
jeongkyojun <kyojun2002@naver.com>
2022-12-08 21:11:48 +0900

    feat : main페이지 일부 진행

--- src/page/Main/index.tsx
+++ src/page/Main/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Image from '../../atom/Image';
+import Text from '../../atom/Text';
 import ButtonGroups from '../../molcule/ButtonGroups';
 import { button_list } from './button_list';
-import { ButtonWrapper, MainHeader, StyledMain } from './styles';
+import { ButtonWrapper, MainHeader, StyledMain, TextWrapper } from './styles';
 
 const Main = () => {
   return (
     <StyledMain>
-      <MainHeader></MainHeader>
+      <MainHeader>
+        <Image src={`/img/user.jpg`} alt={'user'}></Image>
+        <TextWrapper>
+          <Text> 제 이름은 정교준 입니다. </Text>
+        </TextWrapper>
+      </MainHeader>
       <ButtonWrapper>
         <ButtonGroups items={button_list} direction={`row`}></ButtonGroups>
       </ButtonWrapper>
--- src/page/Main/styles.ts
+++ src/page/Main/styles.ts
@@ -4,6 +4,14 @@ export const StyledMain = styled.div`
   display: flex;
 `;
 
-export const MainHeader = styled.div``;
+export const MainHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const ButtonWrapper = styled.div``;
