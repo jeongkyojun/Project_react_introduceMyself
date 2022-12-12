4cb0320ae9dfc7998b370dcdc0622804953a21be
jeongkyojun <kyojun2002@naver.com>
2022-12-12 11:29:07 +0900

    feat: List 컴포넌트 작성중

---
+++ src/atom/List/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { ListStyleTypes } from './types';
+
+export const StyledList = styled.div``;
+
+export const StyledUnorderList = styled.ul<ListStyleTypes>``;
+
+export const StyledOrderList = styled.ol<ListStyleTypes>``;
---
+++ src/atom/List/types.ts
@@ -0,0 +1,6 @@
+export interface ListTypes extends ListStyleTypes {
+  items: string[];
+  isOrder?: boolean;
+}
+
+export interface ListStyleTypes {}
