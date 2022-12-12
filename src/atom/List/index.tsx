5c91460300f88a27e521df8fbd0a3a2a897afa65
jeongkyojun <kyojun2002@naver.com>
2022-12-12 11:25:24 +0900

    feat: list 컴포넌트 작성중

---
+++ src/atom/List/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyledList, StyledOrderList, StyledUnorderList } from './styles';
+import { ListTypes } from './types';
+
+const List = ({ items, isOrder }: ListTypes) => {
+  return <StyledList>{}</StyledList>;
+};
+
+export default List;
