cc1476a3c73fe28d7371b89e76023d942129c517
jeongkyojun <kyojun2002@naver.com>
2022-12-27 19:35:53 +0900

    feat: project 소개 type 변경

--- src/organism/Main/ProjectSection/types.ts
+++ src/organism/Main/ProjectSection/types.ts
@@ -3,8 +3,10 @@ import { ImageTypes } from '../../../atom/Image/types';
 export interface ProjectItemsTypes extends ImageTypes {
   link: string;
   disabled: boolean;
+  explain?: string[];
   handler?: () => void;
 }
 export interface ProjectSectionTypes {
   items: ProjectItemsTypes[];
 }
+
